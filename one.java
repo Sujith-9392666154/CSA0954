23b7af87844ce06cf796629e5cb520f188d89ac3
Sujith-9392666154 <112929829+Sujith-9392666154@users.noreply.github.com>
2022-10-15 23:56:53 +0530

    Create one.java

---
+++ one.java
@@ -0,0 +1,15 @@
+Êþº¾   ? )
+       java/lang/Object <init> ()V   java/util/Scanner	 
+ 
+   
+  java/lang/System in Ljava/io/InputStream;
+      (Ljava/io/InputStream;)V	 
+    out Ljava/io/PrintStream;  Enter name: 
+       java/io/PrintStream print (Ljava/lang/String;)V
+      ! nextLine ()Ljava/lang/String;  # one Code LineNumberTable 
+print_name 
+SourceFile 
+manoj5.java   "            $        *· ±    %         &   $   =     »  Y² 	· L² ¶ +¶ M±    %       	 
+ 
+  
+    '    (
