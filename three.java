8f729fca31ef54cb59a57bcdf32d758457152f26
Sujith-9392666154 <112929829+Sujith-9392666154@users.noreply.github.com>
2022-10-15 23:56:31 +0530

    Create three.java

---
+++ three.java
@@ -0,0 +1,21 @@
+Êþº¾   ? J
+       two <init> ()V   java/util/Scanner	 
+ 
+   
+  java/lang/System in Ljava/io/InputStream;
+      (Ljava/io/InputStream;)V	 
+    out Ljava/io/PrintStream;  Enter the python marks: 
+       java/io/PrintStream print (Ljava/lang/String;)V
+      ! 
+hasNextInt ()Z #  Invalid
+  % &   println
+   ( ) * 
+nextDouble ()D , Enter the c programming marks:  . Enter the Maths marks:    0 1 2 makeConcatWithConstants (D)Ljava/lang/String;  4 three Code LineNumberTable 
+print_mark 
+StackMapTable 
+SourceFile 
+manoj5.java BootstrapMethods =
+ > ?  @ 1 A $java/lang/invoke/StringConcatFactory ˜(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/invoke/CallSite; C Total marks=  InnerClasses  F %java/lang/invoke/MethodHandles$Lookup  H java/lang/invoke/MethodHandles Lookup   3            5        *· ±    6         7   5   ó  
+   {»  Y² 	· L² ¶ +¶ š ² "¶ $±+¶ '9² +¶ +¶ š ² "¶ $±+¶ '9² -¶ +¶ š ² "¶ $±+¶ '9ccI² (º /  ¶ ±    6   N     
+      " " # # % ) & 1 ' 8 ) @ * A , G - O . V 0 ^ 1 _ 3 e 4 n 5 z 6 8    ü #   þ   ü   9    : ;     <  B D   
+  E G I 
